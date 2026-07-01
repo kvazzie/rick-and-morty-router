34eaeb7c2f68fd09b6cf80ae08e9946c891d9325
Rachev Artemii <quzyboy@gmail.com>
2026-07-01 11:18:43 +0300

    chore(build): update vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import { pwaAssets } from './pwa-assets.config';
